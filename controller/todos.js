2b42950425c4b5efe25b5e9757a3c2c917a4a8d6
Bibek Sah <bibekkumarsah.456@gmail.com>
2025-01-13 19:18:20 +0545

    crud

--- controller/todos.js
+++ controller/todos.js
@@ -37,4 +37,34 @@ export const Todo_get=async(req,res)=>{
   console.log(error)
   }
 }
+export const updatetodo=async(req,res)=>{
+  try{
+    const {title}=req.body
+    const todo=await todolist.findByIdAndUpdate({_id:req.params.todoid},{title},{new:true})
+    await todo.save()
+    return res.status(200).json({
+      success:true,
+      todo,
+      message:"updated successfuly"
+    })
+
+  }catch(error){
+    console.log(error)
+  }
+}
+export const deletetodo=async(req,res)=>{
+  try{
+    const todo=await todolist.findByIdAndDelete({_id:req.params.todoid})
+    await todo.save()
+    return res.status(200).json({
+      success:true,
+      todo,
+      message:"deleted successfuly"
+    })
+
+  }catch(error){
+    console.log(error)
+  }
+}
+
 
--- router/todo.js
+++ router/todo.js
@@ -1,6 +1,10 @@
 import express from 'express'
-import { Todo_app, Todo_get } from '../controller/todos.js'
+import { deletetodo, Todo_app, Todo_get, updatetodo } from '../controller/todos.js'
 const todoRouter=express.Router()
 todoRouter.post('/todoitem',Todo_app)
 todoRouter.get('/gettodo',Todo_get)
+todoRouter.put('/gettodo/:todoid',updatetodo)
+todoRouter.delete('/gettodo/:todoid',deletetodo)
+
+
 export default todoRouter
